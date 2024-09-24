bc861c519f080943690c341b32f74da53193e65e
anubistoon <wdlc2003@hotmail.com>
2024-09-24 11:26:25 -0300

    Update simulador.js

--- js/simulador.js
+++ js/simulador.js
@@ -1,15 +1,15 @@
-    let bpm = prompt("Ingrese su frecuencia cardíaca");
-    let resultado = "";
+let bpm = parseInt(prompt("Ingrese su frecuencia cardíaca")); // Convertir a número
+let resultado = "";
 
-    if (bpm > 0) {
-        if (bpm >= 60 && bpm <= 100) {
-            resultado = "Tu frecuencia cardíaca está en el rango normal.";
-        } else if (bpm < 60) {
-            resultado = "Tu frecuencia cardíaca está por debajo del rango normal.";
-        } else {
-            resultado = "Tu frecuencia cardíaca está por encima del rango normal.";
-        }
+if (!isNaN(bpm) && bpm > 0) { // Verificar que bpm es un número y mayor que 0
+    if (bpm >= 60 && bpm <= 100) {
+        resultado = "Tu frecuencia cardíaca está en el rango normal.";
+    } else if (bpm < 60) {
+        resultado = "Tu frecuencia cardíaca está por debajo del rango normal.";
     } else {
-        resultado = "Por favor, ingresa un valor válido.";
-     }   
-    console.log(resultado);
+        resultado = "Tu frecuencia cardíaca está por encima del rango normal.";
+    }
+} else {
+    resultado = "Por favor, ingresa un valor válido.";
+}   
+console.log(resultado);
